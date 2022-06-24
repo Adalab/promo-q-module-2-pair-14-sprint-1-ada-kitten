9008fcdb0643a792b4bce606d0962ee16e86248b
DeliaMesonero <delia_mv88@hotmail.com>
2022-06-24 11:13:47 +0200

    variables

--- js/main.js
+++ js/main.js
@@ -1,7 +1,72 @@
 "use strict";
 
-const newForm = document.querySelector(".js-new-form");
-newForm.classList.remove("collapsed");
+//const newForm = document.querySelector(".js-new-form");
+//newForm.classList.remove("collapsed");
 
-const newList = document.queryselector(".js-list");
+const newList = document.querySelector(".js-list");
 
+const cat1 = `<li class="card">
+<article>
+  <img 
+    class="card_img"
+    src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg"
+    alt="gatito"
+  />
+  <h3 class="card_title">Anastacio</h3>
+  <h4 class="card_race">British Shorthair</h4>
+  <p class="card_description">
+    Ruiseño, juguetón, le guta estar tranquilo y que nadie le
+    moleste. Es una maravilla acariciarle!
+  </p>
+</article>
+</li>`;
+
+const cat2 = `<li class="card">
+<img
+  class="card_img"
+  src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg"
+  alt="gatito"
+/>
+<h3 class="card_title">Fiona</h3>
+<h4 class="card_race">British Shorthair</h4>
+<p class="card_description">
+  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
+  Es una maravilla acariciarle!
+</p>
+</li>`;
+
+const cat3= `<li class="card">
+<img
+  class="card_img"
+  src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
+  alt="gatito"
+/>
+<h3 class="card_title">Cielo</h3>
+<h4 class="card_race">British Shorthair</h4>
+<p class="card_description">
+  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
+  Es una maravilla acariciarle!
+</p>
+</li>`;
+
+newList.innerHTML = cat1 + cat2 + cat3; 
+//newList.innerHTML += cat2;
+//newList.innerHTML += cat3;
+
+let cat1Url= "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
+
+let cat1Name= "Anastacio";
+let cat1Description= "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+let cat1Race="British Shorthair";
+
+let cat2Url="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
+
+let cat2Name="Fiona";
+let cat2Description="Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!";
+let cat2Race="British Shorthair";
+
+
+let cat3Url="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
+let cat3Name="Cielo";
+let cat3Description="Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!";
+let cat3Race="British Shorthair";

7002e46bacb163548afcfa6302652af1d5633599
DeliaMesonero <delia_mv88@hotmail.com>
2022-06-28 11:11:48 +0200

    icon

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const newForm = document.querySelector(".js-new-form");
-newForm.classList.remove("collapsed");
+//newForm.classList.remove("collapsed");
 
 const newList = document.querySelector(".js-list");
 
@@ -9,7 +9,7 @@ let cat1Url= "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
 
 let cat1Name= "Anastacio";
 let cat1Description= "Ruiseño, juguetón, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
-let cat1Race="British Shorthair";
+let cat1Race="";
 
 let cat2Url="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
 
@@ -22,6 +22,12 @@ let cat3Url="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-6
 let cat3Name="Cielo";
 let cat3Description="Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!";
 let cat3Race="British Shorthair";
+let html= '';
+if (cat1Race === "") { html="No se ha especificado la raza";
+  
+} else {
+  html = cat1Race;
+}
 
 const cat1 = `<li class="card">
 <article>
@@ -31,7 +37,7 @@ const cat1 = `<li class="card">
     alt="gatito"
   />
   <h3 class="card_title">${cat1Name}</h3>
-  <h4 class="card_race">${cat1Race}</h4>
+  <h4 class="card_race">${html}</h4>
   <p class="card_description">
     ${cat1Description}
   </p>
@@ -82,3 +88,10 @@ if( cat1Description.includes(descrSearchText) ) {
   resultText.innerHTML = 'Prueba';
   //console.log("Prueba");
   }
+ const icon= document.querySelector(".js-icon");
+ event.preventDefault(icon); 
+ icon.addEventListener( "click" , (event) => {
+  newForm.classList.remove("collapsed");
+ } )
+  
+  
